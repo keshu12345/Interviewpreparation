2a93ff42ad817f8f84cbb59e7944f3e33b4f3df3
keshucscs <keshucscs@gmail.com>
2021-05-29 16:27:59 +0530

    parkinglotSwiggy

---
+++ ParkingLot/src/parkingLot/ParkingLot.java
@@ -0,0 +1,10 @@
+package parkingLot;
+
+public class ParkingLot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ParkingLot/src/parkingLot/VehicleSize.java
@@ -0,0 +1,8 @@
+package parkingLot;
+
+public enum VehicleSize {
+	MoterCycle,large,VeryLarge
+	
+};
+
+
